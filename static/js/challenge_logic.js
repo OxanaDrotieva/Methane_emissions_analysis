0e065457f805cee2958b234155bfec728857d5a6
Aleksandr Drotev <oxanadrotieva@gmail.com>
2022-04-08 21:41:30 -0500

    added GDP and emissions data to the map, broke the map, delete unused file

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -1,6 +1,3 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -29,122 +26,160 @@ let baseMaps = {
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
-let allEarthquakes = new L.LayerGroup();
-let majorEQ = new L.LayerGroup();
+let gdp = new L.LayerGroup();
+let emission = new L.LayerGroup();
+let ratio = new L.LayerGroup();
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
-let overlays = {
-  "Earthquakes": allEarthquakes,
-  "Major Earthquakes": majorEQ
+let groupedOverlays = {
+  "Countries": {"GDP": gdp,
+  "Methane Emission": emission,
+  "Metane Emission per Billion $ of GDP": ratio}
+};
+
+var options = {
+  // Make the "Landmarks" group exclusive (use radio inputs)
+  exclusiveGroups: ["Countries"],
+  // Show a checkbox next to non-exclusive group labels for toggling all
+  groupCheckboxes: true
 };
 
 // Then we add a control to the map that will allow the user to change which
 // layers are visible.
-L.control.GroupedLayers(baseMaps, overlays).addTo(map);
+L.control.groupedLayers(baseMaps, groupedOverlays, options).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+d3.json("https://methane-bucket.s3.amazonaws.com/geojson.json").then(function(data) {
 
   // This function returns the style data for each of the earthquakes we plot on
   // the map. We pass the magnitude of the earthquake into two separate functions
   // to calculate the color and radius.
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
-  }
+  // function styleInfo(feature) {
+  //   return {
+  //     opacity: 1,
+  //     fillOpacity: 1,
+  //     fillColor: getColor(feature.properties.mag),
+  //     color: "#000000",
+  //     // radius: getRadius(feature.properties.mag),
+  //     stroke: true,
+  //     weight: 0.5
+  //   };
+  // }
 
   // This function determines the color of the marker based on the magnitude of the earthquake.
-  function getColor(magnitude) {
-    if (magnitude > 6) {
-      return "maroon";
-    }
-    if (magnitude > 5) {
-      return "#ea2c2c";
-    }
-    if (magnitude > 4) {
-      return "#ea822c";
-    }
-    if (magnitude > 3) {
-      return "#ee9c00";
-    }
-    if (magnitude > 2) {
-      return "#eecc00";
-    }
-    if (magnitude > 1) {
-      return "#d4ee00";
-    }
-    return "#98ee00";
-  }
+  // function getColor(magnitude) {
+  //   if (magnitude > 6) {
+  //     return "maroon";
+  //   }
+  //   if (magnitude > 5) {
+  //     return "#ea2c2c";
+  //   }
+  //   if (magnitude > 4) {
+  //     return "#ea822c";
+  //   }
+  //   if (magnitude > 3) {
+  //     return "#ee9c00";
+  //   }
+  //   if (magnitude > 2) {
+  //     return "#eecc00";
+  //   }
+  //   if (magnitude > 1) {
+  //     return "#d4ee00";
+  //   }
+  //   return "#98ee00";
+  // }
 
   // This function determines the radius of the earthquake marker based on its magnitude.
   // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
-  function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
-    return magnitude * 4;
-  }
-
+  // function getRadius(magnitude) {
+  //   if (magnitude === 0) {
+  //     return 1;
+  //   }
+  //   return magnitude * 4;
+  // }
+  // data = "https://methane-bucket.s3.amazonaws.com/geojson.json"
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
+  L.choropleth(data, {
     	// We turn each feature into a circleMarker on the map.
-    	pointToLayer: function(feature, latlng) {
-      		console.log(data);
-      		return L.circleMarker(latlng);
-        },
+    	valueProperty: 'GDP', // which property in the features to use
+      scale: ['blue', 'yellow'], // chroma.js scale - include as many as you like
+	    steps: 5, // number of breaks or steps in range
+	    mode: 'q', // q for quantile, e for equidistant, k for k-means
+	    style: {
+              color: '#000000', // border color
+              weight: 2,
+              fillOpacity: 0.8
+	    },
+      // function(feature, latlng) {
+      // 		console.log(data);
+      // 		return L.circleMarker(latlng);
+      //   },
       // We set the style for each circleMarker using our styleInfo function.
-    style: styleInfo,
+    // style: styleInfo,
      // We create a popup for each circleMarker to display the magnitude and location of the earthquake
      //  after the marker has been created and styled.
-     onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      onEachFeature: function(feature, layer) {
+              layer.bindPopup("Country: " + feature.properties.name + "<br>GDP: " + feature.properties.GDP);
     }
-  }).addTo(allEarthquakes);
+  }).addTo(gdp);
 
   // Then we add the earthquake layer to our map.
-  allEarthquakes.addTo(map);
+  gdp.addTo(map);
+
+
+  L.choropleth(data, {
+      valueProperty: 'methane.Total including LUCF', // which property in the features to use
+      scale: ['blue', 'yellow'], // chroma.js scale - include as many as you like
+      steps: 5, // number of breaks or steps in range
+      mode: 'q', // q for quantile, e for equidistant, k for k-means
+      style: {
+                color: '#000000', // border color
+                weight: 2,
+                fillOpacity: 0.8
+  },
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("Country: " + feature.properties.name + "<br>GDP: " + feature.properties.methane.Total including LUCF);
+  }
+  }).addTo(emission);
+
+  // Then we add the earthquake layer to our map.
+  emission.addTo(map);
+
 
   // 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
   d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
 
-    // 4. Use the same style as the earthquake data.
-    function styleInfo(feature) {
-      return {
-        opacity: 1,
-        fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
-        color: "#000000",
-        radius: getRadius(feature.properties.mag),
-        stroke: true,
-        weight: 0.5
-      };
-    }    
+    // // 4. Use the same style as the earthquake data.
+    // function styleInfo(feature) {
+    //   return {
+    //     opacity: 1,
+    //     fillOpacity: 1,
+    //     fillColor: getColor(feature.properties.mag),
+    //     color: "#000000",
+    //     radius: getRadius(feature.properties.mag),
+    //     stroke: true,
+    //     weight: 0.5
+    //   };
+    // }    
   
-    // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
-    function getColor(magnitude) {
-      if (magnitude > 6) {
-        return "maroon";
-      }
-      if (magnitude > 5) {
-        return "#ea2c2c";
-      }
-        return "#ea822c" 
-    }
+    // // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+    // function getColor(magnitude) {
+    //   if (magnitude > 6) {
+    //     return "maroon";
+    //   }
+    //   if (magnitude > 5) {
+    //     return "#ea2c2c";
+    //   }
+    //     return "#ea822c" 
+    // }
     
-    // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
-    function getRadius(magnitude) {
-      if (magnitude === 0) {
-        return 1;
-      }
-      return magnitude * 4;
-    }
+    // // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+    // function getRadius(magnitude) {
+    //   if (magnitude === 0) {
+    //     return 1;
+    //   }
+    //   return magnitude * 4;
+    // }
     
     // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
     // sets the style of the circle, and displays the magnitude and location of the earthquake
