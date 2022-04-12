47224d313b2f611db00fcbd523606e97c81b0184
Aleksandr Drotev <oxanadrotieva@gmail.com>
2022-04-11 21:41:55 -0500

    added countries into dropdown menu, added plot for China

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,63 +1,73 @@
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+  var selector = d3.select("#selCountry");
 
-  // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
-    var sampleNames = data.names;
+  d3.json("../geojson.json").then((data) => {   
+    var countries = [];
+    for(var k in data.features) {
+        countries.push(data.features[k].properties.name);
+    }
+    console.log(countries);
+  
+  
+// Use the list of countries to populate the select options
+countries.forEach((country) => {
+  var selectbox = document.getElementById("selCountry");
+  var optn = document.createElement("OPTION");
+  optn.text = country;
+  optn.value = country;
+  selectbox.options.add(optn);
+  console.log(country);
+  // selector.selectAll("option")
+  //   .append("option")
+  //   .text(country)
+  //   .property("value", country);
 
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-
-    // Use the first sample from the list to build the initial plots
-    var firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
+// Use the first sample from the list to build the initial plots
+var firstSample = countries[0];
+// buildCharts(firstSample);
+// buildMetadata(firstSample);
+});
+});
 }
-
 // Initialize the dashboard
 init();
 
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
-  buildMetadata(newSample);
-  buildCharts(newSample);
+  // buildMetadata(newSample);
+  // buildCharts(newSample);
   
 }
 
-// Demographics Panel 
-function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    console.log(data);
-    var metadata = data.metadata;
-    // Filter the data for the object with the desired sample number
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    // Use d3 to select the panel with id of `#sample-metadata`
-    var PANEL = d3.select("#sample-metadata");
+// // Demographics Panel 
+// function buildMetadata(sample) {
+//   d3.json("samples.json").then((data) => {
+//     console.log(data);
+//     var metadata = data.metadata;
+//     // Filter the data for the object with the desired sample number
+//     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+//     var result = resultArray[0];
+//     // Use d3 to select the panel with id of `#sample-metadata`
+//     var PANEL = d3.select("#sample-metadata");
 
-    // Use `.html("") to clear any existing metadata
-    PANEL.html("");
+//     // Use `.html("") to clear any existing metadata
+//     PANEL.html("");
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-    Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    });
+//     // Use `Object.entries` to add each key and value pair to the panel
+//     // Hint: Inside the loop, you will need to use d3 to append new
+//     // tags for each key-value in the metadata.
+//     Object.entries(result).forEach(([key, value]) => {
+//       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+//     });
 
-  });
-}
+//   });
+// }
 
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("../geojson.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
@@ -128,44 +138,44 @@ function buildCharts(sample) {
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
 
     
-    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-    var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    // // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    // var metadata = data.metadata;
+    // var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     
-    // 2. Create a variable that holds the first sample in the metadata array.
-    var result = resultArray[0];
+    // // 2. Create a variable that holds the first sample in the metadata array.
+    // var result = resultArray[0];
 
-    // 3. Create a variable that holds the washing frequency.
-    var wfreq = parseFloat(result.wfreq);
+    // // 3. Create a variable that holds the washing frequency.
+    // var wfreq = parseFloat(result.wfreq);
     
-    // 4. Create the trace for the gauge chart.
-    var gaugeData = [{
-      value: wfreq,
-      title: { text: "<b> Belly Button Washing Frequency </b> <br> Scrubs Per Week" },
-      type: "indicator",
-      mode: "gauge+number",
-      gauge: {
-        axis: { range: [null, 10] },
-        bar: { color: "black" },
-        steps: [
-          { range: [0, 2], color: "red" },
-          { range: [2, 4], color: "orange" },
-          { range: [4, 6], color: "yellow" },
-          { range: [6, 8], color: "yellowgreen" },
-          { range: [8, 10], color: "forestgreen" }
-        ],
-      }
-    }
+    // // 4. Create the trace for the gauge chart.
+    // var gaugeData = [{
+    //   value: wfreq,
+    //   title: { text: "<b> Belly Button Washing Frequency </b> <br> Scrubs Per Week" },
+    //   type: "indicator",
+    //   mode: "gauge+number",
+    //   gauge: {
+    //     axis: { range: [null, 10] },
+    //     bar: { color: "black" },
+    //     steps: [
+    //       { range: [0, 2], color: "red" },
+    //       { range: [2, 4], color: "orange" },
+    //       { range: [4, 6], color: "yellow" },
+    //       { range: [6, 8], color: "yellowgreen" },
+    //       { range: [8, 10], color: "forestgreen" }
+    //     ],
+    //   }
+    // }
      
-    ];
+    // ];
     
-    // 5. Create the layout for the gauge chart.
-    var gaugeLayout = { 
-      automargin: true
-    };
+    // // 5. Create the layout for the gauge chart.
+    // var gaugeLayout = { 
+    //   automargin: true
+    // };
 
-    // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+    // // 6. Use Plotly to plot the gauge data and layout.
+    // Plotly.newPlot("gauge", gaugeData, gaugeLayout);
     
   });
 }
